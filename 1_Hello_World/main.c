a374c5dc910c33c400c04609df087ff971aa02e0
Jason Dreisbach <jtdreisb@gmail.com>
2012-09-24 16:50:00 -0700

    remove getting started and I got the thing working

--- 1_Hello_World/main.c
+++ 1_Hello_World/main.c
@@ -1,8 +1,16 @@
 // Jason Dreisbach
 // Jason Desenza
 // Project 1 (Hello World)
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+void lcd_cmd(unsigned char cmd);
+void lcd_init();
+
 void lcd_cmd(unsigned char cmd)
-{	// Set RS low 
+{	
+	// Set RS low 
 	PORTB &= ~(1<<PB4);
 	//Set E high
 	PORTB |= (1<<PB2);
@@ -18,7 +26,8 @@ void lcd_cmd(unsigned char cmd)
 }
 
 void lcd_init()
-{	DDRB |= 0xFC; 
+{	
+	DDRB |= 0xFC; 
 	DDRD |= 0x1F;
 	PORTB &= ~(1<<PB2);
 	_delay_ms(15);
@@ -29,5 +38,20 @@ void lcd_init()
 
 int main()
 {
+	DDRB |= (1<<PB5);
+    
+    // infinite loop
+    while(1) {
+        // Write PB0 high
+        PORTB |= (1<<PB5);
+        // 250 us
+        _delay_ms(250);
+        // Turn PB0 OFF
+        PORTB &= ~(1<<PB5);
+        // wait for the rest of the second
+        _delay_ms(750);
+    }
 	return 0;
 }
+
+
