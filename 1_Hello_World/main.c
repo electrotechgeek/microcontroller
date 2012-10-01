361e349036d5a87e76ba82d27d44b8486c756457
Jason Dreisbach <jtdreisb@gmail.com>
2012-09-30 20:39:22 -0700

    finished the lab requirements

--- 1_Hello_World/main.c
+++ 1_Hello_World/main.c
@@ -9,30 +9,20 @@
 int main()
 {
 	// DDRB |= (1<<PB5);
-
+    DDRC = 0x00;
+    PORTC = 0xFF;
     lcd_init();
     // _delay_ms(10);
-    lcd_printString("hello world");
-    lcd_lowerLine();
-    lcd_printString("foo bar");
-    _delay_ms(900);
+    
     while(1) {
         lcd_clear();
-        // Write PB0 high
-        lcd_printString("Test");
-        // 250 us
-        _delay_ms(900);
-        lcd_printString(" String");
-        // Turn PB0 OFF
-        // PORTB &= ~(1<<PB5);
-        // lcd_printChar('a');
-        // wait for the rest of the second
-        _delay_ms(750);
-        lcd_clear();
-        lcd_printString("hello world");
-        _delay_ms(1000);
-
-        
+        if (PINC & 0x1) {
+            lcd_printString("hello world");
+        }
+        else {
+            lcd_printString("ouch!");
+        }
+        _delay_ms(500);
     }
 
 	return 0;
